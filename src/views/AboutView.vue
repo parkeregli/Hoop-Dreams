ce1c9203e1b282789f3d3116015188d164f1dacd
Parker Egli <parker.egli22@gmail.com>
2024-03-06 17:09:26 -0700

    Adding Tauri and rust for desktop app

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <Card class="about">
+    <template #title>
+      <h1>About</h1>
+    </template>
+    <template #content>
+      <h1>This is an about page</h1>
+    </template>
+  </Card>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
