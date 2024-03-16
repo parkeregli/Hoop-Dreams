a6d6bebf2e84278e4f3279dea3a14534dce15b6f
Parker Egli <parker.egli22@gmail.com>
2024-03-16 14:43:29 -0600

    Fixing me breaking it

--- src/components/TeamStats.vue
+++ src/components/TeamStats.vue
@@ -1,17 +1,24 @@
 <template>
-  <card>
+  <card class="border-solid border-cyan-600">
     <template #title>
       <h1 class="title">Team Stats</h1>
     </template>
-    <template #content>
-      <DataView :value="teamStats">
+    <template class="flex flex-initial" #content>
+      <DataView class="overflow-scroll min-h-0" :value="teamStats">
         <template #list="slotProps">
           <div class="flex flex-column">
-            <div v-for="(item, index) in slotProps.items" :key="index">
-              <span class="stat"> {{ item.stat }} </span> -
-              <span class="home"> {{ item.home }} </span> -
+            <div
+              class="flex flex-wrap justify-content-between w-full"
+              v-for="(item, index) in slotProps.items"
+              :key="index"
+            >
+              <div class="stat">{{ item.stat }}</div>
 
-              {{ item.away }}
+              <div class="flex gap-7">
+                <span class="home"> {{ item.home }} </span>
+
+                {{ item.away }}
+              </div>
             </div>
           </div>
         </template>
@@ -21,16 +28,11 @@
 </template>
 
 <script setup lang="ts">
-  const props = defineProps({ teamStats: []});
+const props = defineProps({ teamStats: []});
 </script>
 
 <style scoped>
 .title {
   text-align: center;
-}
-
-.card {
-  width: 100%;
-  height: 200px;
 }
 </style>
