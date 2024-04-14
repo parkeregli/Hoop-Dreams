91dc0d139004d9c02cf42ded205a1e81888ac4d5
Matt Becker <matt_becker13@hotmail.com>
2024-04-14 10:18:36 -0600

    updating layout of homePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="flex justify-content-center">
-    <Card class="homeCard">
-      <template #title><h1>Hoop Dreams</h1></template>
-      <template #content>
-        <Menu :model="items">
-          <template #item="{ item, props }">
-            <router-link
-              v-if="item.route"
-              v-slot="{ href, navigate }"
-              :to="item.route"
-              custom
-            >
-              <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-                <span :class="item.icon" />
-                <span class="ml-2">{{ item.label }}</span>
-              </a>
-            </router-link>
-            <a
-              v-else
-              v-ripple
-              :href="item.url"
-              :target="item.target"
-              v-bind="props.action"
-            >
-              <span :class="item.icon" />
-              <span class="ml-2">{{ item.label }}</span>
-            </a>
-          </template>
-        </Menu>
+    <Menu
+      :model="items"
+      class="align-items-center h-full h-25rem text-3xl homeCard p-6"
+      :pt:label:class="'mb-4'"
+      ><template #start
+        ><h1 class="inline-flex align-items-center text-7xl">
+          Hoop Dreams
+        </h1></template
+      >
+      <template #item="{ item, props }">
+        <router-link
+          v-if="item.route"
+          v-slot="{ href, navigate }"
+          :to="item.route"
+          custom
+        >
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+        <a
+          v-else
+          v-ripple
+          :href="item.url"
+          :target="item.target"
+          v-bind="props.action"
+        >
+          <span :class="item.icon" />
+          <span class="ml-2">{{ item.label }}</span>
+        </a>
       </template>
-    </Card>
+    </Menu>
   </div>
 </template>
 
@@ -42,37 +45,35 @@ const router = useRouter();
 const items = ref([
   {
     label: "Coach Game",
-    icon: "pi pi-palette",
+    icon: "pi pi-caret-right",
     command: () => {
       router.push("/game");
     },
   },
   {
     label: "Career Mode",
-    icon: "pi pi-link",
+    icon: "pi pi-caret-right",
   },
   {
     label: "Options",
-    icon: "pi pi-home",
+    icon: "pi pi-caret-right",
   },
 
   {
     label: "Exit",
-    icon: "pi pi-home",
+    icon: "pi pi-sign-out",
   },
 ]);
 </script>
 
-<style>
+<style scoped>
 .homeCard {
   width: 400px;
   height: 600px;
   align-items: center;
 }
 
-body {
-  display: flex;
-  align-items: center;
-  background-color: steelblue;
+.menuCard {
+  list-style-type: none;
 }
 </style>
